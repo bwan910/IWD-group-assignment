5d2bc75777dba34c09d17bb8e78190647ef173fd
bwan910 <bwan910@gmail.com>
2021-06-21 21:45:50 +0800

    Added UI and functions

--- aboutus.php
+++ aboutus.php
@@ -1,19 +1,51 @@
-<!DOCTYPE html>
+<?php 
+  include 'includes/header.php'; 
+?>
+
+
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About Us</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
   <!-- Bootstrap core CSS -->
   <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<!-- Material Design Bootstrap -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/css/mdb.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/css/mdb.min.css" rel="stylesheet">
 </head>
-<body>
-   This is an About Us
+
+<body style="background-color: white;">
+  <div style="background-color: #133e6b  " class="jumbotron">
+    <div class="primary text-center">
+      <h1 style="color:#00a7e1" class="display-3">About Us</h1>
+      <p style="color:white" class="lead">This is a modified jumbotron that occupies
+        the entire horizontal space of its parent.</p>
+    </div>
+  </div>
+  <div class="container">
+    <p style="color:#000000; max-width: 700px; margin: auto; padding-bottom: 50px;" class="lead text-center justify-center">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Quisque id diam vel quam elementum pulvinar etiam non quam. Pellentesque diam volutpat commodo
+      sed. Nisl vel pretium lectus quam id leo in vitae. Quis lectus nulla at volutpat diam. Lacinia quis vel eros
+      donec ac odio tempor orci. Sed viverra ipsum nunc aliquet bibendum enim facilisis. Id aliquet risus feugiat in
+      ante metus. Quis viverra nibh cras pulvinar mattis nunc sed blandit. Vestibulum morbi blandit cursus risus at
+      ultrices. Leo a diam sollicitudin tempor id. Montes nascetur ridiculus mus mauris. Adipiscing vitae proin
+      sagittis nisl rhoncus mattis rhoncus urna neque. Amet justo donec enim diam vulputate ut. Massa massa
+      ultricies mi quis hendrerit dolor magna eget est. Mi quis hendrerit dolor magna eget est lorem ipsum. Placerat
+      orci nulla pellentesque dignissim enim.
+    </p>
+
+  </div>
 </body>
+
 </html>
+
+<?php 
+  include 'employer/includes/footer.php'; 
+?>
--- contactus.php
+++ contactus.php
@@ -21,28 +21,128 @@
 
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/css/mdb.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet" />
+
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
 </head>
 
 <body>
+    <script>
+        //function submit_form() {
+        //  document.contact.submit();
+        //document.contact - form.reset();
+        window.onbeforeunload = () => {
+            for (const form of document.getElementsByTagName("form")) {
+                form.reset();
+            }
+        }
+        //}
 
-    <h1>Contact Us</h1>
-    <form action="https://formspree.io/f/xpzkrbag" method="POST">
-        <label>
-            Your email:
-            <input type="email" name="_replyto">
-        </label><br>
-        <label>
-            Your message:
-            <textarea name="message"></textarea>
-        </label><br>
+    </script>
+    <section class="mb-4">
 
-        <!-- your other form fields go here -->
+        <!--Section heading-->
+        <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+        <!--Section description-->
+        <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us
+            directly. Our team will come back to you within
+            a matter of hours to help you.</p>
 
-        <button type="submit">Send</button>
-    </form>
-    <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15887.931668026793!2d100.322455!3d5.4195653!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa9ad76b9c61d0bd3!2sUOW%20Malaysia%20KDU%20Penang%20University%20College!5e0!3m2!1sen!2smy!4v1624089311753!5m2!1sen!2smy"
-        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <div style="max-width: 1100px; margin: auto;" class="row">
+
+            <!--Grid column-->
+            <div class="col-md-9 mb-md-0 mb-5">
+                <form id="contact" name="contact" action="https://formspree.io/f/xpzkrbag" method="POST">
+
+                    <!--Grid row-->
+                    <div class="row">
+                        <!--Grid column-->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="name" name="name" placeholder="Your name" class="form-control"
+                                    required>
+                            </div>
+                        </div>
+                        <!--Grid column-->
+
+                        <!--Grid column-->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="email" name="email" placeholder="Email" class="form-control" required>
+                            </div>
+                        </div>
+                        <!--Grid column-->
+
+                    </div>
+                    <!--Grid row-->
+
+                    <!--Grid row-->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="md-form mb-0">
+                                <input type="text" id="subject" name="subject" placeholder="Subject"
+                                    class="form-control" required>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Grid row-->
+
+                    <!--Grid row-->
+                    <div class="row">
+
+                        <!--Grid column-->
+                        <div class="col-md-12">
+
+                            <div class="md-form">
+                                <textarea type="text" id="message" name="message" rows="2"
+                                    class="form-control md-textarea" placeholder="Your message" required></textarea>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!--Grid row-->
+                    <div class="text-center text-md-left">
+                        <button class="btn btn-primary text-center" name="submit" type="submit">Send</button>
+                    </div>
+                </form>
+
+
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div style="padding-right: 20px;" class="col-md-3 text-center">
+                <ul class="list-unstyled mb-0">
+                    <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                        <p>San Francisco, CA 94126, USA</p>
+                    </li>
+
+                    <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                        <p>+ 01 234 567 89</p>
+                    </li>
+
+                    <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                        <p>contact@gmail.com</p>
+                    </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+        </div>
+    </section>
+
 </body>
 
+
 </html>
+
+
+<br><br>
+<iframe
+    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15887.931668026793!2d100.322455!3d5.4195653!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa9ad76b9c61d0bd3!2sUOW%20Malaysia%20KDU%20Penang%20University%20College!5e0!3m2!1sen!2smy!4v1624089311753!5m2!1sen!2smy"
+    width="100%" height="350" style="border:10;" allowfullscreen="" loading="lazy"></iframe>
